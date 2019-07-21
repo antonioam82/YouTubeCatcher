aad5492b6a7a3375353bbc8fdfa80e0712a06f6e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2019-07-21 18:49:28 +0200

    Update YouTube_Catcher.py

--- YouTube_Catcher.py
+++ YouTube_Catcher.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
@@ -24,11 +26,12 @@ def descarga():
         filename = s.download()
         messagebox.showinfo("FIN DE DESCARGA","Descarga finalizada con éxito")
     except:
-        messagebox.showwarning("ERROR","Se ha iterrumpido la descarga")
+        messagebox.showwarning("ERROR","Se produjo un error inesperado")
     
 
 Entry(ventana,font=('Arial',15,'bold'),textvariable=URLL,width=30).place(x=196,y=130)
-Button(ventana,width=20,text="DIRECTORIO",bg="pale green",command=direc).place(x=287,y=270)
+Button(ventana,width=20,text="CAMBIAR DIRECTORIO",bg="pale green",command=direc).place(x=287,y=270)
 Button(ventana,width=20,text="DESCARGA",bg="pale green",command=descarga).place(x=287,y=310)
+Label(ventana,width=12,text="URL de video",bg="navajo white").place(x=314,y=109)
 
 ventana.mainloop()
